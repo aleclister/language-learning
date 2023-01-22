ef8c2cef08172d921d6cb99ef35a5e1a1cc52a79
aleclister <alec.lister@gmail.com>
2023-01-22 18:16:46 +0100

    Finished Navbar, although still need to implement the hamburger menu on small screens

--- components/Hero.js
+++ components/Hero.js
@@ -7,10 +7,10 @@ const Hero = () => {
     return (
         <div className=" mx-6 my-6">
             <div className="bg-[#fff5f1] px-6 lg:px-16 py-10 rounded-3xl">
-                {/* <Navbar /> */}
+                <Navbar />
                 <div className="flex flex-col gap-5 md:justify-between items-center md:flex-row">
                     <div className="flex flex-col gap-5  md:gap-10">
-                        <p className="text-2xl pt-16 font-medium lg:text-6xl sm:text-4xl md:text-5xl">
+                        <p className="text-2xl pt-8 lg:pt-16 font-medium lg:text-6xl sm:text-4xl md:text-5xl">
                             A unique approach <br /> to learning foreign <br /> languages online
                         </p>
                         <p className="text-sm sm:text-base lg:text-lg text-gray-500"> We offer online conversation classes that are <br className="md:flex hidden"/>personalised to fit your needs.
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,24 +1,55 @@
 import { React } from 'react'
 import Image from 'next/image'
 import logoImage from "../public/assets/logo.png"
+// import Script from "../styles/script"
+
 
 
 const Navbar = () => {
     return (
         <div className=''>
-            <div className="flex justify-between items-center">
+            <div className="flex flex-col gap-5 md:justify-between items-center md:flex-row">
                 <Image className='w-64 px-5' src={logoImage} alt="Logo" />
-                <div className="flex items-center gap-14">
-                    <p>Home</p>
-                    <p>Our teachers</p>
-                    <p>How it works</p>
+
+                <div className="hidden md:flex flex-col gap-5 md:justify-between items-center md:flex-row">
+                    <a href="/">Home</a>
+                    <a href="#">Our teachers</a>
+                    <a href="#">How it works</a>
                 </div>
-                <button className="btn bg-white text-blue-300 border-none px-5 hover:bg-blue-400 hover:text-white capitalize rounded-full">
+                <button className="btn hidden md:block bg-white text-blue-300 border-none px-5 hover:bg-blue-400 hover:text-white capitalize rounded-full">
                     Get Started
                 </button>
+                
+                {/* Hamburger Menu */}
+                <button id="menu-btn" class="block hamburger md:hidden focus:outline-none">
+                    <span className="hamburger-top"></span>
+                    <span className="hamburger-middle"></span>
+                    <span className="hamburger-bottom"></span>
+                    
+                </button>
+
+                {/* Mobile Menu */}
+                <div className="md:hidden">
+                    <div id="menu" class="absolute flex-col items-center self-end hidden py-8 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
+                        <a href="#">Home</a>
+                        <a href="#">Our Teachers</a>
+                        <a href="#">How it works</a>
+                    </div>
+                </div>
             </div>
         </div>   
     )
 }
+
+
+// const btn = document.getElementById('menu-btn')
+// const nav = document.getElementById('menu')
+
+// btn.addEventListener('click', () => {
+//     btn.classList.toggle('open')
+//     nav.classList.toggle('flex')
+//     nav.classList.toggle('hidden')
+// })
+
 
 export default Navbar 
